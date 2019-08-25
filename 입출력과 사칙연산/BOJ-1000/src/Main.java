82959a0c02f7ee5fb4d8481719687657271fb150
ChanHeeGid <l01083582454@gmail.com>
2019-08-25 17:17:08 +0900

    두 수의 합

---
+++ 입출력과 사칙연산/BOJ-1000/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int A;
+        int B;
+        Scanner scanner = new Scanner(System.in);
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        scanner.close();
+        System.out.println(A+B);
+	}
+
+}
