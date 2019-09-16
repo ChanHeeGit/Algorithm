5605d2b0c4fffc54443ef440b44b868a8a33b0c9
ChanHeeGit <l01083582454@gmail.com>
2019-09-17 00:43:53 +0900

    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성, 테스트 케이스 T만큼 반복

---
+++ for문/BOJ-10950/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int Case = scanner.nextInt();
+        for(int i=0; i<Case; i++){
+           int A = scanner.nextInt();
+           int B = scanner.nextInt();
+           System.out.println(A+B);
+            }
+        }
+
+    }
