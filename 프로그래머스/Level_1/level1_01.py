fbb18c4fa9f1c48876b4af62ef8379a389d56787
CHANHEE <l01083582454@gmail.com>
2021-02-14 21:27:36 +0900

    문자열 다루기 기본

---
+++ 프로그래머스/Level_1/level1_01.py
@@ -0,0 +1,10 @@
+# 프로그래머스 LEVEL1 
+# 문자열 다루기 기본
+
+def solution(s):
+    answer = True
+    if( (len(s) == 4 or len(s) == 6) and s.isdigit() == True ) :
+        answer = True
+    else :
+        answer = False
+    return answer
